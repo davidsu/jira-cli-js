7051ed6804546df16c8de1f76f7a12fc37e8bf3c
David Susskind <david.susskind@walkme.com>
2022-02-19 11:54:19 +0200

    highlight matches

--- source/components/Fzf.tsx
+++ source/components/Fzf.tsx
@@ -1,30 +1,63 @@
+/* eslint-disable no-case-declarations */
 import React, { useState, useMemo, useEffect, useCallback } from 'react'
 import { Text, Box } from 'ink'
 import TextInput from './TextInput'
 import Fuse from 'fuse.js'
 import useScreenSize from './useScreenSize.js'
+import chalk from 'chalk'
 
-function makeDisplayList(
-  list: unknown[],
+function createMatchedTextNode(fuseItem: Fuse.FuseResult<string>, width: number) {
+  let coloredItem = fuseItem.item.substring(0, width)
+  if (fuseItem.matches?.length) {
+    const indices = [...fuseItem.matches[fuseItem.matches.length - 1].indices].sort(([sa], [sb]) => sa - sb)
+    let last = Number.POSITIVE_INFINITY
+    for (let i = indices.length - 1; i >= 0; i--) {
+      let [start, finish] = indices[i]
+      finish = Math.min(finish, last - 1)
+      last = Math.min(start, last)
+      if (finish < width && finish - start > 1) {
+        coloredItem =
+          coloredItem.substring(0, start) +
+          chalk.green(coloredItem.substring(start, finish + 1)) +
+          coloredItem.substring(finish + 1)
+      }
+    }
+  }
+  return coloredItem
+}
+
+function createRow(fuseItem: Fuse.FuseResult<string>, width: number, focusId: string) {
+  let coloredItem = createMatchedTextNode(fuseItem, width)
+  const props = fuseItem.item === focusId ? { backgroundColor: '#303030', color: '#FFFFFF' } : {}
+  return (
+    <Box key={fuseItem.item} flexGrow={4}>
+      <Text {...props}>{coloredItem}</Text>
+    </Box>
+  )
+}
+
+const createRows = (
+  list: Fuse.FuseResult<string>[],
   focusedIdx: number,
   focusId: string,
   width: number,
   height: number
-): React.ReactNode {
+) =>
+  list
+    .slice(Math.max(focusedIdx - height + 3, 0), Math.max(height - 2, focusedIdx + 1))
+    .map(fuseItem => createRow(fuseItem, width, focusId))
+
+function makeDisplayList(
+  list: Fuse.FuseResult<string>[],
+  focusedIdx: number,
+  focusId: string,
+  width: number,
+  height: number
+) {
   const fillHeight = Math.max(height - 3 - list.length, 0)
   return (
     <>
-      {list.slice(Math.max(focusedIdx - height + 3, 0), Math.max(height - 2, focusedIdx + 1)).map(txt => (
-        <Box key={txt as string} flexGrow={4}>
-          {txt === focusId ? (
-            <Text backgroundColor="#303030" color="#FFFFFF">
-              {txt.substring(0, width)}
-            </Text>
-          ) : (
-            <Text>{(txt as string).substring(0, width - 5)}</Text>
-          )}
-        </Box>
-      ))}
+      {createRows(list, focusedIdx, focusId, width, height)}
       <Box height={fillHeight} />
     </>
   )
@@ -43,37 +76,49 @@ function debounce(func, timeout = 300) {
 const Fzf = ({ list, header }: { list: Array<string>; header: string }) => {
   const [query, setQuery] = useState('')
   const [focusId, setFocusId] = useState('')
-  const [filteredList, setFilteredList] = useState(list)
+  const [filteredList, setFilteredList] = useState([] as Fuse.FuseResult<string>[])
   const { height, width } = useScreenSize()
 
   const fuse = useMemo(
     () =>
-      new Fuse(list, { useExtendedSearch: true, includeMatches: true, ignoreLocation: true, ignoreFieldNorm: true }),
+      new Fuse(list, {
+        useExtendedSearch: true,
+        includeMatches: true,
+        ignoreFieldNorm: true,
+        distance: 3000,
+      }),
     [list]
   )
-  const p = useCallback(
+  const applyQuery = useCallback(
     debounce(() => {
-      let result = list
-      const _query = (query || '').replace(/'\s*$/, '')
+      const _query = (query || '')
+        .replace(/'\s*$/, '')
+        .replace(/(\w+(?:\\\s\w+)+)/g, (_match, g1) => `"${g1.replace(/\\/g, '')}"`) //make it behave like fzf
       if (fuse.search && _query) {
-        result = fuse.search(_query).map(({ item }) => item) as string[]
+        setFilteredList(fuse.search(_query))
+        return
       }
-      setFilteredList(result)
+      setFilteredList(list.map(item => ({ item })) as Fuse.FuseResult<string>[])
     }, 90),
     [list, fuse, query]
   )
-  useEffect(p, [list, fuse, query])
-
-  const focusedIdx = useMemo(() => Math.max(filteredList.indexOf(focusId), 0), [filteredList, focusId])
+  useEffect(applyQuery, [list, fuse, query])
+  const indexOfFocusId = useMemo(
+    () => filteredList.findIndex(fuseItem => fuseItem.item === focusId),
+    [focusId, filteredList]
+  )
+  const focusedIdx = useMemo(() => Math.max(indexOfFocusId, 0), [indexOfFocusId])
 
   function onCombo(input, key) {
+    //todo handle delete, ctrl-u, arrows
     if (key.ctrl) {
       switch (input) {
         case 'n':
-          setFocusId(filteredList[filteredList.indexOf(focusId) + 1] || '')
+          setFocusId(filteredList[indexOfFocusId + 1]?.item || '')
           break
         case 'p':
-          setFocusId(filteredList[filteredList.indexOf(focusId) - 1] || filteredList[filteredList.length - 1] || '')
+          const idx = indexOfFocusId > 0 ? indexOfFocusId - 1 : filteredList.length - 1
+          setFocusId(filteredList[idx]?.item || '')
           break
       }
     }
